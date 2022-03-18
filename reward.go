b66e7a8d30585349b040aae760059d71adf82bd7
alanal <alanal@qq.com>
2022-03-18 18:12:51 +0800

    0

--- reward.go
+++ reward.go
@@ -8,17 +8,17 @@ type RewardService interface {
 }
 
 type Reward struct {
-	RewardId       int64  `json:"user_id,omitempty"`
-	CustomerId     string `json:"customer_id,omitempty"`
-	Email          string `json:"email,omitempty"`
-	FirstName      string `json:"first_name,omitempty"`
-	LastName       string `json:"last_name,omitempty"`
-	PhotoUrl       string `json:"photo_url,omitempty"`
-	About          string `json:"about,omitempty"`
-	Birthdate      string `json:"birthdate,omitempty"`
-	ProfileAddress string `json:"profile_address,omitempty"`
-	Rewardname     string `json:"username,omitempty"`
-	Privacy        string `json:"privacy,omitempty"`
-	Points         string `json:"points,omitempty"`
-	ReferLink      string `json:"refer_link,omitempty"`
+	RewardId int64 `json:"user_id,omitempty"`
+	// CustomerId     string `json:"customer_id,omitempty"`
+	// Email          string `json:"email,omitempty"`
+	// FirstName      string `json:"first_name,omitempty"`
+	// LastName       string `json:"last_name,omitempty"`
+	// PhotoUrl       string `json:"photo_url,omitempty"`
+	// About          string `json:"about,omitempty"`
+	// Birthdate      string `json:"birthdate,omitempty"`
+	// ProfileAddress string `json:"profile_address,omitempty"`
+	// Rewardname     string `json:"username,omitempty"`
+	// Privacy        string `json:"privacy,omitempty"`
+	// Points         string `json:"points,omitempty"`
+	// ReferLink      string `json:"refer_link,omitempty"`
 }
